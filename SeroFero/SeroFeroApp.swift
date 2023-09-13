ef5c9f77119d1af7af0430d0a9f42e2d602160ef
Rishi-2028 <rishibhandari2028@gmail.com>
2023-09-13 16:18:33 +0545

    Initial Commit

---
+++ SeroFero/SeroFeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeroFeroApp.swift
+//  SeroFero
+//
+//  Created by Rishi on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SeroFeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
